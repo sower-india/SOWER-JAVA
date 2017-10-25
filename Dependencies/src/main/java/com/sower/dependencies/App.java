7f8649e23c2265fa9963a86e9355efc5f9bb5ade
svaduka <sainagaraju.vaduka@gmail.com>
2017-10-25 19:58:43 +0530

    Created Project Dependencies for adding all dependencies for the SOWER project
    Change-Id: I4ea3df157e4392d05e3b34147853e3fedcdefeb6
    Signed-off-by: sa356713 <sa356713@10.156.239.172>

---
+++ Dependencies/src/main/java/com/sower/dependencies/App.java
@@ -0,0 +1,13 @@
+package com.sower.dependencies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
